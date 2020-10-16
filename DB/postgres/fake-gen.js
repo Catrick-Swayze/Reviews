05a57755204c256d194d7c2b5e562b2b9d969a71
Nathaniel Schwartz <nathanielhschwartz@gmail.com>
2020-10-16 19:13:20 -0400

    composed dummy method for recursively generating data for postgres

---
+++ DB/postgres/fake-gen.js
@@ -0,0 +1,33 @@
+const { Client } = require('pg');
+var client = new Client({ database: 'postgres' });
+
+const recursiveQuery = function(productId, reviewId, numReviews, firstReview) {
+  client.query('insert into testing (productId, reviewId) values ($1, $2)', [productId, reviewId], () => {
+    if (reviewId < numReviews) {
+      recursiveQuery(productId, reviewId + 1, numReviews, false);
+    } else if (productId < 10) {
+      recursiveQuery(productId + 1, 1, Math.floor(Math.random() * 5 + 1), true);
+    } else {
+      client.end();
+      console.log('donzsoo!');
+    }
+  });
+};
+
+client.connect();
+
+client.query('create database nstest', [], (err, results) => {
+  client.end();
+
+  client = new Client({ database: 'nstest' });
+  client.connect();
+
+  client.query('CREATE TABLE IF NOT EXISTS testing (id serial primary key, productId int, reviewId int)', [], (err) => {
+    if (err) {
+      client.end();
+      console.log('1: ' + err);
+    } else {
+      recursiveQuery(1, 1, Math.floor(Math.random() * 5 + 1), true);
+    }
+  });
+});
---
+++ DB/postgres/generateData.js
@@ -0,0 +1,44 @@
+const { Client } = require('pg');
+const faker = require('faker');
+const fs = require('fs');
+
+const getRandomNum = function(min, max) {
+  return Math.floor((Math.random() * (max - min) + min));
+};
+
+const getRandomDateString = function() {
+  const months = {
+    Jan: '01',
+    Feb: '02',
+    Mar: '03',
+    Apr: '04',
+    May: '05',
+    Jun: '06',
+    Jul: '07',
+    Aug: '08',
+    Sep: '09',
+    Oct: '10',
+    Nov: '11',
+    Dec: '12'
+  };
+  var date = faker.date.past().toString().split(' ');
+  return `${date[3]}-${months[date[1]]}-${date[2]}`;
+};
+
+const generateReviewParams = function(productId) {
+  return [
+    faker.name.firstName(),
+    getRandomNum(0, 6),
+    faker.lorem.paragraph(),
+    getRandomDateString(),
+    faker.random.boolean(),
+    faker.random.words(),
+    getRandomNum(0, 6),
+    getRandomNum(0, 6),
+    getRandomNum(0, 6),
+    getRandomNum(1, 4),
+    ["null"],
+    getRandomNum(0, 21),
+    productId
+  ].join(',');
+};
